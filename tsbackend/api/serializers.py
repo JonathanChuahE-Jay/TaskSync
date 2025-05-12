a73343327354fcba5c32e6109b25c71cf19d3e28
Jonathan chuah <jonathanchuah5889@gmail.com>
2025-05-12 15:32:04 +0800

    fixed issues

--- tsbackend/api/serializers.py
+++ tsbackend/api/serializers.py
@@ -23,7 +23,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         data['id'] = self.user.id
         data['username'] = self.user.username
         data['email'] = self.user.email
-        data['first_name'] = self.user.first_na
+        data['first_name'] = self.user.first_name
         data['last_name'] = self.user.last_name
         data['phone_number'] = self.user.phone_number
         data['role'] = self.user.role
